1b7fb66daff7b0935a49b3cc61432a656fb108d8
AshanSandeep06 <ashansandeep06@gmail.com>
2022-04-28 10:30:56 +0530

    Aded POS System

---
+++ Layered_Architecture/src/dao/CustomerDAOImpl.java
@@ -0,0 +1,53 @@
+package dao;
+
+import db.DBConnection;
+import model.CustomerDTO;
+import view.tdm.CustomerTM;
+
+import java.sql.*;
+import java.util.ArrayList;
+
+public class CustomerDAOImpl {   //Impl - Implementation
+    public ArrayList<CustomerDTO> getAllCustomers() throws SQLException, ClassNotFoundException {
+        Connection connection = DBConnection.getDbConnection().getConnection();
+        Statement stm = connection.createStatement();
+        ResultSet rst = stm.executeQuery("SELECT * FROM Customer");
+        ArrayList<CustomerDTO> allCustomers = new ArrayList<>();
+
+        while(rst.next()){
+            allCustomers.add(new CustomerDTO(
+                    rst.getString(1),
+                    rst.getString(2),
+                    rst.getString(3)
+            ));
+        }
+        return allCustomers;
+    }
+
+    public boolean saveCustomer(CustomerDTO dto) throws SQLException, ClassNotFoundException {
+        Connection connection = DBConnection.getDbConnection().getConnection();
+        PreparedStatement pstm = connection.prepareStatement("INSERT INTO Customer (id,name, address) VALUES (?,?,?)");
+        pstm.setString(1, dto.getId());
+        pstm.setString(2, dto.getName());
+        pstm.setString(3, dto.getAddress());
+        return pstm.executeUpdate() > 0;
+    }
+
+    public boolean updateCustomer(CustomerDTO dto) throws SQLException, ClassNotFoundException {
+        Connection connection = DBConnection.getDbConnection().getConnection();
+        PreparedStatement pstm = connection.prepareStatement("UPDATE Customer SET name=?, address=? WHERE id=?");
+        pstm.setString(1, dto.getName());
+        pstm.setString(2, dto.getAddress());
+        pstm.setString(3, dto.getId());
+        return pstm.executeUpdate() > 0;
+    }
+
+    public boolean customerIsExist(String id) throws SQLException, ClassNotFoundException {
+        Connection connection = DBConnection.getDbConnection().getConnection();
+        PreparedStatement pstm = connection.prepareStatement("SELECT id FROM Customer WHERE id=?");
+        pstm.setString(1, id);
+        return pstm.executeQuery().next();
+    }
+
+
+}
