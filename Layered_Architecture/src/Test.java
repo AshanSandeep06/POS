3b474e9bf3bf254fc368c37264e6a9d54002949d
AshanSandeep06 <ashansandeep06@gmail.com>
2022-05-25 14:53:05 +0530

    Content Updated

--- Layered_Architecture/src/Test.java
+++ Layered_Architecture/src/Test.java
@@ -1,15 +1,11 @@
+import java.math.BigDecimal;
 import java.sql.SQLException;
 
 public class Test {
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
 
-        /*CrudDAO c1 = new CustomerDAOImpl();
-        c1.save("Ashan");
-
-
-        CrudDAO<CustomerDTO,String> c2 = new CustomerDAOImpl();
-        c2.save(new CustomerDTO());*/
-
+        BigDecimal unitPrice=new BigDecimal(200.00).setScale(2);
+        System.out.println(unitPrice);
 
     }
 }
